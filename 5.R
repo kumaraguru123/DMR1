46db15b0b6d8f5feb62de1aab1b40b50f6081b0b
kumaraguru123 <79808476+kumaraguru123@users.noreply.github.com>
2024-10-05 12:45:37 -0700

    Add files via upload

---
+++ 5.R
@@ -0,0 +1,9 @@
+library(datarium)
+head(marketing)
+cor(marketing$youtube,marketing$facebook)
+cov(marketing)
+cor(marketing$youtube,marketing$facebook)
+cor(marketing)
+hist(marketing$youtube)
+
+correlation and covariance
